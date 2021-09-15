cdb1f2665e730226dd19151911f58b6c7a20bf1a
common <commom@163.com>
2021-09-15 20:24:30 +0800

    day two third commit

--- serviceProvider8001/src/main/java/com/cxs/springbootWork/controller/PaymentController.java
+++ serviceProvider8001/src/main/java/com/cxs/springbootWork/controller/PaymentController.java
@@ -4,9 +4,12 @@ import com.cxs.springbootWork.entity.CommonResult;
 import com.cxs.springbootWork.entity.Payment;
 import com.cxs.springbootWork.service.PaymentService;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 public class PaymentController {
@@ -16,6 +19,9 @@ public class PaymentController {
 
     @Value("${server.port}")
     private String port;
+
+    @Resource
+    private DiscoveryClient discoveryClient;
 
     @PostMapping("/payment/create")
     public CommonResult create(@RequestBody Payment payment) {
@@ -37,6 +43,22 @@ public class PaymentController {
         }else {
             return new CommonResult(404, "未查询到任何结果", null);
         }
+    }
+
+    @GetMapping("/payment/discovery")
+    public Object getServiceInfo() {
+        List<String> services = discoveryClient.getServices();
+
+        for (String service : services) {
+            System.out.println(service);
+        }
+        List<ServiceInstance> instanceList = discoveryClient.getInstances("serviceProvider");
+
+        for (ServiceInstance instance : instanceList) {
+            System.out.println(instance.getServiceId()+"\t"+instance.getHost()+"\t"+instance.getPort()+"\t"+instance.getUri());
+        }
+
+        return this.discoveryClient;
     }
 
 }
