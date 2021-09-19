d0c9c78729814ee0d71480c45bc148da6ebe7c70
common <commom@163.com>
2021-09-19 09:43:16 +0800

    day six first commit

---
+++ gateway9527/src/main/java/com/cxs/springbootWork/filter/GatewayFilter.java
@@ -0,0 +1,30 @@
+package com.cxs.springbootWork.filter;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+public class GatewayFilter implements GlobalFilter, Ordered {
+    //参数exchange可以理解为request，带了一个过滤链chain
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        String uname = exchange.getRequest().getQueryParams().getFirst("uname");
+        if (uname == null) {
+            System.out.println("用户名不能为空");
+            exchange.getResponse().setStatusCode(HttpStatus.NOT_ACCEPTABLE);
+            return exchange.getResponse().setComplete();
+        }
+        return chain.filter(exchange);
+    }
+
+    //执行顺序
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
